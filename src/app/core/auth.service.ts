7629e334cefdf626d47719a63a52d132480b12d9
Ismail Koksal <ismail@koksal.fr>
2020-02-03 17:46:24 +0100

    Create method to get current connected user

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -1,25 +1,32 @@
 import {Injectable} from '@angular/core';
 import {AngularFireAuth} from '@angular/fire/auth';
 import * as firebase from 'firebase';
+import {Observable} from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthService {
+    user$: Observable<firebase.User | null>;
 
-  constructor(private afAuth: AngularFireAuth) {
-  }
+    constructor(private afAuth: AngularFireAuth) {
+        this.user$ = afAuth.user;
+    }
 
-  signInWithEmailAndPassword(email: string, password: string): Promise<firebase.auth.UserCredential> {
-    return this.afAuth.auth.signInWithEmailAndPassword(email, password);
-  }
+    getUser(): Observable<firebase.User> {
+        return this.user$;
+    }
 
-  createUserWithEmailAndPassword(email: string, password: string): Promise<firebase.auth.UserCredential> {
-    return this.afAuth.auth.createUserWithEmailAndPassword(email, password);
-  }
+    signInWithEmailAndPassword(email: string, password: string): Promise<firebase.auth.UserCredential> {
+        return this.afAuth.auth.signInWithEmailAndPassword(email, password);
+    }
 
-  signOut(): void {
-    this.afAuth.auth.signOut();
-  }
+    createUserWithEmailAndPassword(email: string, password: string): Promise<firebase.auth.UserCredential> {
+        return this.afAuth.auth.createUserWithEmailAndPassword(email, password);
+    }
+
+    signOut(): void {
+        this.afAuth.auth.signOut();
+    }
 
 }
