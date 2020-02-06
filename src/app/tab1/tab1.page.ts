bb1880ac2aef2d6ef7f212d12745ff98050d398e
aksel <tajaouartm@gmail.com>
2020-02-06 22:23:59 +0100

    display today's Tours

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,19 +1,46 @@
 import {Component} from '@angular/core';
 import {NavController} from '@ionic/angular';
+import {TourService} from '../core/tour.service';
+import {Observable} from 'rxjs';
+import {Tour} from '../models/tour';
+import {map} from 'rxjs/operators';
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+    selector: 'app-tab1',
+    templateUrl: 'tab1.page.html',
+    styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
 
-  segment = 'card';
+    segment = 'card';
+    public tours$: Observable<Tour[]>;
+    public today_tours$: Observable<Tour[]>;
 
-  constructor(public navCtrl: NavController) {
-  }
-  segmentChanged(ev: any) {
-    console.log('Segment changed', ev);
-  }
+
+    constructor(public navCtrl: NavController, private tourService: TourService) {
+        this.tours$ = this.tourService.getTours();
+        this.today_tours$ = this.tours$.pipe(
+            map(tours => tours.filter(tour => this.isToday(tour.date.toDate())))
+        );
+
+    }
+
+    segmentChanged(ev: any) {
+        console.log('Segment changed', ev);
+    }
+
+
+    isToday(someDate: Date) {
+        const today = new Date();
+        let tourDate: Date;
+        tourDate = new Date(someDate);
+        console.log(today.getMonth());
+        console.log(tourDate.getMonth());
+        return someDate.getDate() === today.getDate() &&
+            someDate.getMonth() === today.getMonth() &&
+            someDate.getFullYear() === today.getFullYear();
+    }
+
+   
 
 }
