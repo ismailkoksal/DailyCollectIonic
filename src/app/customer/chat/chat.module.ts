22892cc70e399a94821405a4bd0735e34d220734
Ismail Koksal <ismail@koksal.fr>
2020-02-12 10:03:33 +0100

    Create chat Scroll to bottom after user send a message

--- src/app/customer/chat/chat.module.ts
+++ src/app/customer/chat/chat.module.ts
@@ -7,15 +7,17 @@ import {IonicModule} from '@ionic/angular';
 import {ChatPageRoutingModule} from './chat-routing.module';
 
 import {ChatPage} from './chat.page';
+import {SharedModule} from '../../shared/shared.module';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    ChatPageRoutingModule
-  ],
-  declarations: [ChatPage]
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        ChatPageRoutingModule,
+        SharedModule
+    ],
+    declarations: [ChatPage]
 })
 export class ChatPageModule {
 }
--- src/app/customer/chat/chat.page.ts
+++ src/app/customer/chat/chat.page.ts
@@ -1,4 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {ChatService} from '../../core/chat.service';
+import {Message} from '../../models/message';
+import {Observable} from 'rxjs';
+import {IonContent} from '@ionic/angular';
 
 @Component({
     selector: 'app-chat',
@@ -6,11 +10,31 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./chat.page.scss'],
 })
 export class ChatPage implements OnInit {
+    messages$: Observable<Message[]>;
+    messageContent = '';
+    @ViewChild(IonContent, {static: false}) content: IonContent;
 
-    constructor() {
+    constructor(private chatService: ChatService) {
     }
 
-    ngOnInit() {
+    ngOnInit(): void {
+        this.messages$ = this.chatService.getMessages('ZGvdnepxLvMqhuZNhWD5WEohFcf1');
+    }
+
+    sendMessage(content: string) {
+        if (this.messageContent.trim()) {
+            this.chatService.sendMessage('ZGvdnepxLvMqhuZNhWD5WEohFcf1', content).subscribe(
+                () => {
+                    this.messageContent = '';
+                    this.scrollBottom();
+                },
+                error => console.error(error)
+            );
+        }
+    }
+
+    scrollBottom() {
+        this.content.scrollToBottom(500);
     }
 
 }
